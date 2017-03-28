0fa564efede2f43bef747e9245c2e3257dba5111
Artyom <art231188@gmail.com>
2017-03-28 17:40:25 +0300

    Revert "gotoGroupspage varifi in method"
    This reverts commit 49b0108a13ece463f42ffe7474ef3f5ed16fcef6.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -12,11 +12,6 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupsPage() {
-    if(isElementPresent(By.cssSelector("h1"))
-            && wd.findElement(By.cssSelector("h1")).getText().equals("Groups")
-            && isElementPresent(By.xpath("*//input[1][@value='New group']"))){
-      return;
-    }
     click(By.linkText("groups"));
   }
   public void goToHomePage(){
